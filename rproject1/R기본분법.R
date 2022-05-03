aebcd61fe20b0f53a253a41f4197f4c00435714a
kwak-dong-su <srs3576@gmail.com>
2022-05-03 18:20:06 +0900

    R Project

---
+++ rproject1/R기본분법.R
@@ -0,0 +1,18 @@
+print('안녕하세요.')
+
+a<-100 #할당연산자
+b<-200
+
+print(a*b)
+
+install.packages("glue")
+
+d<-1:10
+print(d)
+
+e<-c(1,3,7,8)
+print(e)
+
+f<-c(1, 3:5, 8:10)
+print(f)
+
---
+++ rproject1/산술연산.R
@@ -0,0 +1,16 @@
+## 변수에 2개의 값을 넣고, 사칙연산과 더한 값의 절대값
+## 더한 결과값의 루트값을 구해서 실행해보세요.
+## 나머지를 구해보세요.
+## packages탭 사용해서 ggplot2 설치
+## cowsay 설치 명령어 사용해서 설치
+
+
+a<-50
+b<-30
+print(a+b)
+print(abs(a+b))
+print(sqrt(a+b))
+print(a%%b)
+
+install.packages("cowsay")
+
